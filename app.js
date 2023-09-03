64ee349dea6159a6d6feca0432ba3a67e743d2b0
arizali-21 <arizali8246@gmail.com>
2023-09-03 14:15:19 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+// Chapter#1
+
+
+alert ("Welcome to my website")
+
+alert("Error! Please enter a valid password")
+
+alert("Welcome to JS world...\nHappy Coding!")
+
+alert("Welcome to JS Land")
+
+alert("Happy Coding!\n▢ prevent this page from creating addtional dialogs.")
+
+alert("hello i can run js through my brower's console")
+
+console.log("hello i can run js through my brower's console")
+
+
+// Chapter#2
+
+var username
+
+var myname = ("Ariz Ali")
+
+var message = ("Hello World")
+
+alert (message)
+
+var fullname = ("Jhone Doe")
+
+alert (fullname)
+
+var age = ("21 Years Old")
+
+alert (age)
+
+var results = ("Certified Mobile Application Development")
+
+alert (results)
+
+var pizzas = ("PIZZA\nPIZZ\n\PIZ\nPI\nP")
+
+alert (pizzas)
+
+var email = (" arizali8246@gmail.com")
+var emailinfo = ("My Email Address is")
+
+alert (emailinfo+email)
+
+
+var book = ("A Smarter Way to learn Javascript")
+var bookinfo = ("I am trying to learn from the book ")
+
+alert (bookinfo+book)
+
+document.write("<h1>Yah! I can write HTML content through JAVASCRIPT</h1>")
+
+alert ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
